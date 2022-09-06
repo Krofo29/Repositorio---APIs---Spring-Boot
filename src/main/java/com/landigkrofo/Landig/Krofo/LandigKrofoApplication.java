cf689e49be724bfe2e3c761062b2ce4ed10e9f5a
Krofo29 <89664110+Krofo29@users.noreply.github.com>
2022-09-06 17:59:40 -0300

    Repositorio APIs - Spring Boot
    Repositorio Back End con el desarrollo de la APIs - Spring Boot

---
+++ src/main/java/com/landigkrofo/Landig/Krofo/LandigKrofoApplication.java
@@ -0,0 +1,13 @@
+package com.landigkrofo.Landig.Krofo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandigKrofoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LandigKrofoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/landigkrofo/Landig/Krofo/LandigKrofoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.landigkrofo.Landig.Krofo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LandigKrofoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
